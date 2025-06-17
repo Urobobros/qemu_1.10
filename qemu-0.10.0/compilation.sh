ec5e201813415288feb791a447db46b63c62f43a
Urobobros <martin.burianek1987@gmail.com>
2025-06-17 17:23:22 +0000

    add alsa in compilation

--- qemu-0.10.0/compilation.sh
+++ qemu-0.10.0/compilation.sh
@@ -1,5 +1,5 @@
 export CFLAGS="-m32 -DDEBUG_CDAUDIO"
 export LDFLAGS="-m32 -no-pie"
-./configure --target-list=i386-softmmu  --enable-cdaudio
+./configure --target-list=i386-softmmu  --enable-cdaudio --audio-drv-list=alsa
 make clean
 make
